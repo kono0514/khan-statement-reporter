c2fa028f775dc7afb0a995851a3783d81934f4bc
Uuganbaatar <uuganbaatarbaynsuren2016@gmail.com>
2021-03-12 20:50:09 +0800

    Generate source map for release builds and upload to sentry

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+
 module.exports = {
   configureWebpack: {
     externals: {
       bufferutil: 'bufferutil',
       'utf-8-validate': 'utf-8-validate',
     },
+    plugins: [],
   },
   pluginOptions: {
     electronBuilder: {
@@ -11,12 +14,32 @@ module.exports = {
       builderOptions: {
         publish: [{
           provider: 'github',
-          releaseType: 'release',
+          releaseType: 'draft',
         }],
         appId: 'com.kono.khan_statement_reporter',
         productName: 'Khan Statement Reporter',
         artifactName: '${productName} Setup ${version}.${ext}',
         afterAllArtifactBuild: './after_build.js',
+      },
+      chainWebpackMainProcess: (config) => {
+        const args = require('yargs/yargs')(process.argv.slice(2)).argv;
+        if (args.p === 'always') {
+          /// Production build going to get published
+          /// Generate source map and upload it to sentry
+
+          config.merge({ devtool: 'source-map' });
+          config.plugin('SentryWebpackPlugin').use(SentryWebpackPlugin, [{
+            // sentry-cli configuration
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+            org: 'kono-ha',
+            project: 'khan-statement-reporter',
+            release: `${require('./package.json').name}@${require('./package.json').version}`,
+    
+            // webpack specific configuration
+            include: './dist_electron/bundled/',
+            urlPrefix: 'app:///',
+          }]);
+        }
       },
       chainWebpackRendererProcess: (config) => {
         config.plugin('html').tap((args) => {
